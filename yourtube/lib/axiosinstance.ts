40c19980d5859fa02c7999888d5a2e29b81fd5e9
Mohit Choudhary <choudharymohit206@gmail.com>
2026-08-07 22:01:37 +0530

    Fix backend URL for deployment

--- yourtube/lib/axiosinstance.ts
+++ yourtube/lib/axiosinstance.ts
@@ -2,10 +2,14 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    process.env.NEXT_PUBLIC_BACKEND_URL || process.env.BACKEND_URL || "http://localhost:5000",
+    process.env.NEXT_PUBLIC_BACKEND_URL ||
+    "http://localhost:5000" ||"https://yourtube-j2cd.onrender.com",
+
   headers: {
     "Content-Type": "application/json",
   },
+
+  withCredentials: true,
 });
 
 export default axiosInstance;
